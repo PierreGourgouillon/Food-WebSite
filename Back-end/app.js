099bd2148112110a13f637aab758d8f002b91e73
pierre <Pierre_gourgouillon@hotmail.com>
2021-07-01 14:49:34 +0200

    Login and register finish, need token

---
+++ Back-end/app.js
@@ -0,0 +1,24 @@
+const express =  require('express')
+const app = express()
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const authentificationRoutes = require("./routes/authentificationUser")
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+mongoose.connect('mongodb+srv://Admin:GN6whb3ocXlzjeHg@cluster0.9x5zu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(bodyParser.json())
+
+app.use("/api/authentification", authentificationRoutes)
+
+module.exports = app
---
+++ Back-end/routes/authentificationUser.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+const authentificationCtrl = require("../controllers/authentificationUser")
+
+router.post("/login",authentificationCtrl.login)
+router.post("/signup", authentificationCtrl.signup)
+
+module.exports = router
