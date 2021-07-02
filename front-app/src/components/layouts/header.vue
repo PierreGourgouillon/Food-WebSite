1cd6a385aa025a424ab013c31423afeaf8a60faf
pierre <Pierre_gourgouillon@hotmail.com>
2021-07-02 14:41:17 +0200

    -Create app Vue -Create route Home

---
+++ front-app/src/components/layouts/header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: "header"
+}
+</script>
+
+<style scoped>
+
+  header{
+    border: 1px solid black;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+    padding: 0;
+    max-width: 150px;
+    height: 100vh;
+  }
+
+</style>
---
+++ front-app/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="rowFlex">
+    <Header></Header>
+  </div>
+</template>
+
+<script>
+import Header from "../components/layouts/header"
+
+export default {
+  name: "Home",
+  components:{
+    Header
+  }
+}
+</script>
+
+<style scoped>
+.rowFlex{
+  display: flex;
+  flex-direction: row;
+  flex-basis: auto;
+  padding: 0;
+  margin: 0;
+}
+</style>
