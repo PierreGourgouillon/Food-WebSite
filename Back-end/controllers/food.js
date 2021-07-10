621210f7921493cec7c83c0cdaed5fc467e88df1
pierre <Pierre_gourgouillon@hotmail.com>
2021-07-11 00:16:28 +0200

    -Update food -Get all foods and only one

--- Back-end/controllers/food.js
+++ Back-end/controllers/food.js
@@ -16,15 +16,37 @@ exports.create = (req, res, next) => {
 }
 
 exports.getFoods = (req, res, next) => {
-
+    Food.find({})
+        .then((foods) => {
+            if (!foods){
+                return res.status(401).json({ error: "Foods not found"})
+            }
+            res.status(201).json({ data: foods })
+        })
+        .catch( error => res.status(500).json({ error }))
 }
 
 exports.getFood = (req, res, next) => {
-
+    Food.findById(req.params.id)
+        .then((food) => {
+            if (!food){
+                return res.status(401).json({ error: "Food not found" })
+            }
+            res.status(201).json({ data: food })
+        })
+        .catch( error => res.status(500).json({ error }))
 }
 
 exports.modifFood = (req, res, next) => {
+    Food.findOneAndUpdate(req.params.id, req.body, { new: true, useFindAndModify: false })
+        .then((food)=> {
+            if (!food){
+                return res.status(401).json({ error: "Food not update" })
+            }
 
+            res.status(201).json({ data: food })
+        })
+        .catch( error => res.status(500).json({ error }))
 }
 
 exports.delete = (req, res, next) => {
